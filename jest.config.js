dd43abcbaa8dd6be0f178fa879a5bf486ca82377
Rami Pellumbi <98100379+ramipellumbi@users.noreply.github.com>
2024-09-25 17:47:10 -0800

    Remove unused files

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  moduleFileExtensions: ["ts", "js"],
-  testMatch: ["**/*.test.ts"],
-  transform: {
-    "^.+\\.ts$": "ts-jest",
-  },
-};
