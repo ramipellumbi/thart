5a2ee4133089b2f132152938c269f4b10e486a21
Rami Pellumbi <98100379+ramipellumbi@users.noreply.github.com>
2024-10-17 20:31:37 -0400

    Fix bug where timeout was undefined if not specified.
    License docs

--- src/worker.ts
+++ src/worker.ts
@@ -1,3 +1,27 @@
+/* --------------------------------------------------------------------------
+   MIT License
+
+   Copyright (c) 2024 Rami Pellumbi
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+ -----------------------------------------------------------------------------*/
+ 
 import type { ShutdownManager } from "./async-shutdown";
 import type { NormalizedThartOptions } from "./types";
 
@@ -14,7 +38,7 @@ export async function startWorker(options: NormalizedThartOptions, manager: Shut
   if (workerConfig.startupTimeoutMs) {
     await Promise.race([
       new Promise((_, reject) =>
-        setTimeout(() => reject(new Error("Worker function timed out")), workerConfig.startupTimeoutMs),
+        setTimeout(() => reject(new Error("Worker function timed out")), workerConfig.startupTimeoutMs ?? STARTUP_TIMEOUT_MS),
       ),
       workerConfig.start(idx),
     ]);
